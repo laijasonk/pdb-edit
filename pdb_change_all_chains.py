1e4cf39710ff7b6465237a7f2af294b555980df8
Jason K Lai <jason.lai@bcm.edu>
2017-07-02 16:33:48 -0500

    Keep only specified chain identifiers in PDB
    Included a new script that strips all lines in the input PDB that do not match
    the specified chain identifiers. Multiple chain identifiers can be specified
    and only matching ATOM, HETATM, and TER lines will be output.

---
+++ pdb_change_all_chains.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.4
+
+"""Changes every line in the PDB file to a single specified chain character."""
+
+import argparse
+
+__author__ = 'Jason K Lai'
+__contact__ = 'http://www.jasonlai.com/'
+
+class pdb_change_all_chains( object ):
+    def __init__( self, args=None ):
+        """The class constructor."""
+        self.pdb = args.pdb if args is not None else None
+        self.chain = args.chain if args is not None else None
+        return
+
+    def run( self ):
+        """The main function."""
+        if self.pdb is None:
+            raise ValueError( 'Missing input PDB.' )
+        if self.chain is None:
+            raise ValueError( 'Missing input chain identifier.' )
+        if len( self.chain ) > 1:
+            raise ValueError( 'Chain identifier must be a single character.' )
+
+        with open( self.pdb, 'r' ) as pdb_handle:
+            for line in pdb_handle:
+                if line[0:4] == 'ATOM' or line[0:6] == 'HETATM':
+                    print( self.force_line_to_chain( line, self.chain ).strip() )
+                else:
+                    print( line.strip() ) # script keeps all other lines intact
+
+        return 
+    
+    def force_line_to_chain( self, in_line, in_chain ):
+        """Force input line to be chain."""
+        out_line = ''
+        try:
+            # standard PDB syntax has the chain on column 22
+            out_line = in_line[ :21 ] + str( in_chain ) + in_line[ 22: ]
+        except:
+            raise ValueError( 'Incomplete ATOM or HETATM line.' )
+            
+        return out_line
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description=__doc__ )
+    parser.add_argument( '-p', '--pdb', dest='pdb', required=True, help='input pdb file (including path if necessary)', type=str )
+    parser.add_argument( '-c', '--chain', dest='chain', required=True, help='single character chain identifier', type=str )
+    main = pdb_change_all_chains( args=parser.parse_args() )
+    main.run()
---
+++ pdb_keep_only_specified_chains.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.4
+
+"""Only keep ATOM or HETATM lines from specified chain identifiers (single or multiple)."""
+
+import argparse
+
+__author__ = 'Jason K Lai'
+__contact__ = 'http://www.jasonlai.com/'
+
+class pdb_keep_only_specified_chains( object ):
+    def __init__( self, args=None ):
+        """The class constructor."""
+        self.pdb = args.pdb if args is not None else None
+        self.chains = args.chains if args is not None else None
+        return
+
+    def run( self ):
+        """The main function."""
+        if self.pdb is None:
+            raise ValueError( 'Missing input PDB.' )
+        if self.chains is None:
+            raise ValueError( 'Missing input chain identifiers.' )
+
+        with open( self.pdb, 'r' ) as pdb_handle:
+            for line in pdb_handle:
+                if line[0:4] == 'ATOM' or line[0:6] == 'HETATM' or line[0:3] == 'TER':
+                    if self.line_contains_chain( line, self.chains ):
+                        print( line.strip() )
+                    else:
+                        pass # did not find the correct chain identifier on this line
+                else:
+                    pass # ignoring all non ATOM or HETATM lines
+
+        return 
+    
+    def line_contains_chain( self, in_line, in_chains ):
+        """Check if the input line contains the specified chain(s)."""
+        try:
+            # standard PDB syntax has the chain on column 22 (21 when index starts at 0)
+            for chain in in_chains:
+                if len( in_line ) > 21:
+                    if in_line[21] == chain:
+                        return True
+                    else:
+                        pass # did not contain correct chain identifier
+                else:
+                    pass # ignore if line isn't even 21 characters long
+        except:
+            raise ValueError( 'Invalid ATOM or HETATM line.' )
+            
+        return False
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description=__doc__ )
+    parser.add_argument( '-p', '--pdb', dest='pdb', required=True, help='input pdb file (including path if necessary)', type=str )
+    parser.add_argument( '-c', '--chains', dest='chains', required=True, help='string of all chain identifiers to keep (e.g. "ADF")', type=str )
+    main = pdb_keep_only_specified_chains( args=parser.parse_args() )
+    main.run()
