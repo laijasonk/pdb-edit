bfe446b7d9d95bfb758aa04395fbd6d1ed9b442c
Jason K Lai <jason.lai@bcm.edu>
2017-07-03 10:40:00 -0500

    H2O and noatom options for pdb_filter_lines.py
    In the filtering script, added in two new option flags for hiding the protein
    ATOM lines and for showing only lines with water coordinates (either ATOM or
    HETATM).

--- pdb_filter_lines.py
+++ pdb_filter_lines.py
@@ -11,24 +11,28 @@ class pdb_atom_hetatm_only( object ):
     def __init__( self, args=None ):
         """The class constructor."""
         self.pdb = args.pdb if args is not None else None
+        self.noatom = args.noatom if args is not None else None
         self.hetatm = args.hetatm if args is not None else None
         self.ter = args.ter if args is not None else None
         self.end = args.end if args is not None else None
         self.model = args.model if args is not None else None
+        self.water = args.water if args is not None else None
         return
 
     def run( self ):
         """The main function."""
         if self.pdb is None:
             raise ValueError( 'Missing input PDB.' )
+        if self.noatom is None: self.noatom = False
         if self.hetatm is None: self.hetatm = False
         if self.ter is None: self.ter = False
         if self.end is None: self.end = False
         if self.model is None: self.model = False
+        if self.water is None: self.water = False
 
         with open( self.pdb, 'r' ) as pdb_handle:
             for line in pdb_handle:
-                if line[0:4] == 'ATOM':
+                if not self.noatom and line[0:4] == 'ATOM':
                     print( line.strip() )
                 elif self.hetatm and line[0:6] == 'HETATM':
                     print( line.strip() )
@@ -38,6 +42,9 @@ class pdb_atom_hetatm_only( object ):
                     print( line.strip() )
                 elif self.model and ( line[0:5] == 'MODEL' or line[0:6] == 'ENDMDL' ):
                     print( line.strip() )
+                elif self.water and ( line[17:20] == 'HOH' or line[17:20] == 'WAT' 
+                                   or line[17:20] == 'H2O' or line[17:20] == 'TP3' ):
+                    print( line.strip() )
                 else:
                     pass # ignoring all other lines
 
@@ -46,9 +53,11 @@ class pdb_atom_hetatm_only( object ):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( description=__doc__ )
     parser.add_argument( '-p', '--pdb', dest='pdb', required=True, help='input pdb file (including path if necessary)', type=str )
-    parser.add_argument( '-a', '--hetatm', dest='hetatm', required=False, help='show HETATM lines', default=False, action='store_true' )
+    parser.add_argument( '-na', '--noatom', dest='noatom', required=False, help='hide ATOM lines', default=False, action='store_true' )
+    parser.add_argument( '-he', '--hetatm', dest='hetatm', required=False, help='show HETATM lines', default=False, action='store_true' )
     parser.add_argument( '-t', '--ter', dest='ter', required=False, help='show TER lines', default=False, action='store_true' )
     parser.add_argument( '-e', '--end', dest='end', required=False, help='show END lines', default=False, action='store_true' )
     parser.add_argument( '-m', '--model', dest='model', required=False, help='show MODEL lines', default=False, action='store_true' )
+    parser.add_argument( '-w', '--water', dest='water', required=False, help='show water lines', default=False, action='store_true' )
     main = pdb_atom_hetatm_only( args=parser.parse_args() )
     main.run()
